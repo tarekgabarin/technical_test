33c6d0dcc2d85dcc4c285e86be144c32cddcbbe7
tarekgabarin <tarekgabarin1992@gmail.com>
2020-01-04 16:41:08 -0500

    added info text

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -6,8 +6,16 @@ const Title = styled.span`
     text-align: center;
     font-family: 'HelveticaNeue-Medium';
     font-weight: "Medium";
+    margin-top: 1em;
+    margin-bottom: .5em;
     font-size: 28px;
     letter-spacing: 0;
+`
+
+const IntroText = styled.p`
+    font-family: 'HelveticaNeue-Medium';
+    font-size: 16px;
+    color: #808080;
 `
 
 
@@ -16,7 +24,7 @@ function AboutPage(){
     return (
         <section>
             <Header/>
-            <section>
+            <section className='container'>
 
                 <div class="flex flex-column">
 
@@ -25,7 +33,11 @@ function AboutPage(){
                         Our Team
                     </Title>
 
-                    
+                    <div>
+                        <IntroText>
+                        Aenean efficitur ac nulla eget cursus. Morbi eget sem id lectus sollicitudin pharetra vitae vel lectus. Fusce placerat mollis malesuada. Cras nisl diam, posuere sit amet magna eu, dictum consequat ante. Mauris blandit enim velit, sodales luctus nunc facilisis eu. Sed ligula tellus, condimentum vel urna vel, faucibus pretium ante. Vivamus mollis commodo nisl nec tempus    
+                        </IntroText>   
+                    </div>
 
                     
 
