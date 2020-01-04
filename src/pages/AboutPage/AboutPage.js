7a4b1b774155214b6b4d36897af76add68e8b7f7
tarekgabarin <tarekgabarin1992@gmail.com>
2020-01-04 16:01:14 -0500

    added title

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,13 +1,38 @@
 import React  from 'react';
+import styled from 'styled-components';
 import Header from '../../components/Header/Header'
+
+const Title = styled.span`
+    text-align: center;
+    font-family: 'HelveticaNeue-Medium';
+    font-weight: "Medium";
+    font-size: 28px;
+    letter-spacing: 0;
+`
+
 
 function AboutPage(){
 
     return (
         <section>
-            <Header>
-        
-            </Header>
+            <Header/>
+            <section>
+
+                <div class="flex flex-column">
+
+                    
+                    <Title>
+                        Our Team
+                    </Title>
+
+                    
+
+                    
+
+
+                </div>
+
+            </section>
         </section>
     )
 
